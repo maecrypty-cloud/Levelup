dd6dede2133f1ea2d970a33a7165144792cd617e
maecrypty-cloud <maecrypty@gmail.com>
2025-07-22 13:03:55 +0530

    Add files via upload

---
+++ character_list.dart
@@ -0,0 +1,7 @@
+import '../models/character_model.dart';
+
+final List<Character> characters = [
+  Character(name: 'Itachi', levelUnlock: 1),
+  Character(name: 'Garo', levelUnlock: 2),
+  Character(name: 'Levi', levelUnlock: 3),
+];
