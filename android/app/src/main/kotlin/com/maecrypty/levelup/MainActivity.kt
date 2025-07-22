0485820e71680a47391616816addef114110a85b
maecrypty-cloud <maecrypty@gmail.com>
2025-07-22 18:20:58 +0530

    Update and rename MainActivity.kt to MainActivity.kt

--- android/app/src/main/kotlin/com/maecrypty/levelup/MainActivity.kt
+++ android/app/src/main/kotlin/com/maecrypty/levelup/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.habit.levelup  // ✅ Change this to your actual package name
+package com.maecrypty.levelup  // ✅ Change this to your actual package name
 
 import io.flutter.embedding.android.FlutterActivity
 
